b395f3208fb10cc6b3146c4173c81decf0fe09fc
vcai122 <vincent.delaware@gmail.com>
2022-08-04 16:17:07 -0400

    add appjs to commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.scss'
 import '../components/template/template.css'
 import '../components/loader/loader.css'
+import '../components/streak/streak.css'
+import '../components/missions/missions.css'
+import '../styles/login.css'
+import '../styles/signup.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
