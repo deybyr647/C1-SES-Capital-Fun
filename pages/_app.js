2e7b10a86c302ef254cc5597b8321754cbeb5203
vcai122 <vincent.delaware@gmail.com>
2022-08-04 16:27:36 -0400

    add css file to app

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.scss'
 import '../components/template/template.css'
 import '../components/loader/loader.css'
 import '../components/streak/streak.css'
+import '../components/missions/missions.css'
 import '../styles/login.css'
 import '../styles/signup.css'
 
