95c2199627814a11363a4f5ac508d3eb5d823c98
vcai122 <vincent.delaware@gmail.com>
2022-08-04 17:24:19 -0400

    redo home component structure

---
+++ components/home/Balances.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Balances() {
+  return (
+    <div>
+      Balance
+    </div>
+  )
+}
+
+export default Balances
--- components/home/Home.js
+++ components/home/Home.js
@@ -1,28 +1,12 @@
 import React from 'react'
+import Balances from './Balances'
+import Goals from './Goals'
 
 function Home() {
   return (
     <div>
      <Balances/>
      <Goals/>
-    </div>
-  )
-}
-
-
-function Balances() {
-  return (
-    <div>
-      Balances
-    </div>
-  )
-}
-
-function Goals() {
-  // no data for this, just fake it
-  return (
-    <div>
-      Goals
     </div>
   )
 }
