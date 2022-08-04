39f9716379216e2a681d43504570c83b74ffb8b5
vcai122 <vincent.delaware@gmail.com>
2022-08-04 19:46:43 -0400

    make balance component

--- components/balance/Balance.js
+++ components/balance/Balance.js
@@ -1,9 +1,67 @@
 import React from 'react'
+import { MdSend, MdChangeCircle, MdMoreHoriz, MdTimer} from 'react-icons/md'
+
+
+const financeInfo = {
+  accountNumber: '0123456701234567',
+  balance: 1200.75,
+  creditScore: '730',
+  type: 'Checking'
+}
 
 function Balance() {
   return (
-    <div>
-      show balance details
+    <div className="balance-details">
+      <div className="account-number">
+        <div>Account Number:</div>
+        <div>{financeInfo.accountNumber}</div>
+      </div>
+      <div className="balance">
+        <h1><span>$</span>{financeInfo.balance}</h1>
+        <h2>Account type: {financeInfo.type.toLocaleUpperCase()}</h2>
+      </div>
+      <div className="functionality">
+        <div>
+          <MdSend/>
+          <div>Pay</div>
+        </div>
+        <div>
+          <MdChangeCircle/>
+          <div>Transfer</div>
+        </div>
+        <div>
+          <MdMoreHoriz/>
+          <div>More</div>
+        </div>
+      </div>
+      <div className="transactions">
+        <div className="top-row">
+          <h4><MdTimer/> Recent Transactions</h4>
+          <h4>View More</h4>
+        </div>
+
+        <div className="element">
+          <div>
+            <div>Recent Transaction</div>
+            <div>Recent Transaction</div>
+          </div>
+          <div>
+            <div>$20.21</div>
+            <div>${financeInfo.balance}</div>
+          </div>
+        </div>
+        <div className="element">
+          <div>
+            <div>Recent Transaction</div>
+            <div>Recent Transaction</div>
+          </div>
+          <div>
+            <div>-$15.46</div>
+            <div>${financeInfo.balance-20.21}</div>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
